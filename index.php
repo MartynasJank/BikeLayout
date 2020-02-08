27c98b640db87e53ff4ec758cc1f01e72a8990a4
Martynas <martynasjank@gmail.com>
2020-02-08 18:24:34 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Uzduotis</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        
+        <header>
+            <div class="inner">
+                <ul>
+                    <li><a href="">About us</a></li>
+                    <li><a href="">Work</a></li>
+                    <li class="logo"><a href=""><img src="images/logo.png" alt="logo"></a></li>
+                    <li><a href="">Shop</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+                <div class="menu">
+                    <img src="images/menu.png">
+                </div>
+            </div>
+                
+        </header>
+        
+        <section class="aboutUs">
+            <div class="inner">
+                <img src="images/icon.png" alt="icon">
+                <h3>A NEW GENERATION OF VINTAGE BIKE</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
+            </div>
+        </section>
+            
+        <section class="gallery">
+            <div class="first">
+                <div class="leftSide">
+                    <img src="images/pic1.png">
+                </div>
+                <div class="rightSide">
+                    <img class="icon" src="images/icon2.png" alt="icon">
+                    <h4>vintage Oliva</h4>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.</p>
+                    <div class="arrow">
+                        <img class="img_arrow" src="images/small.png"> 
+                        <img class="seat" src="images/seat.png" alt="seat">
+                    </div>       
+                    </div>
+            </div>
+            <div class="two">
+                <div class="leftSide">
+                    <img class="icon" src="images/icon3.png" alt="icon">
+                    <h4>La Boriosa</h4>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.</p>
+                    <div class="arrow">
+                        <img class="img_arrow" src="images/small.png"> 
+                        <img class="seat" src="images/seat2.png" alt="seat">
+                    </div>   
+                </div>
+                <div class="rightSide">
+                    <img src="images/pic2.png">
+                </div>
+            </div>
+            <div class="three">
+                <div class="third">
+                    <img src="images/pic3.png">
+                </div>
+                <div class="third">
+                    <img class="icon" src="images/icon4.png" alt="icon">
+                    <h4>retrò Bike - M. Hulot</h4>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived.</p>
+                    <div class="arrow">
+                        <img class="img_arrow" src="images/small.png"> 
+                        <img class="seat" src="images/seat3.png" alt="seat">
+                    </div>    
+                </div>
+                <div class="third"></div>
+            </div>         
+        </section>
+        
+        <section class="social">
+            <div class="inner">
+                <img class="twitter" src="images/twitter.png" alt="Twitter">
+                <h5>AOD New York @aod <span>/ 35 min</span></h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut tellus ac nulla semper rhoncus. Nullam a odio porttitor, dictum turpis vitae, pretium ante amet.</p>
+                <img class="dots" src="images/dots.png">
+            </div>
+        </section>
+        
+        <section class="shop">
+            <div class="pictures">
+                <div class="third one">
+                    <div class="picone"></div>
+                    <div class="pictwo"></div>
+                    <div class="picthree"></div>
+                </div>
+                <div class="third two">
+                    <div class="picone"></div>
+                    <div class="pictwo"></div>
+                </div>
+                <div class="third three">
+                    <div class="picone">
+                        <img src="images/imgtext.png">
+                    </div>
+                    <div class="pictwo"></div>
+                    <div class="picthree"></div>
+                </div>
+            </div>
+            <div class="button">
+                <button>Discover the shop</button>
+            </div>
+        </section>
+        
+        <section class="email">
+            <div class="inner">
+                <img src="images/logo.png">
+                <h6>Stay on the saddle!</h6>
+                <form>
+                    <input type="email" placeholder="enter your email..." required>
+                    <button>GO</button>
+                </form>
+            </div>
+        </section>
+                    
+            <footer>
+                <div class="inner">
+                    <div class="inline third address">
+                        <h6>AOD New York</h6>
+                        <address>
+                            2250 Lexington Avenue<br>
+                            New York, NY 10035 - info@domain.com
+                        </address>
+                    </div>
+                    <div class="inline third links">
+                        <img src="images/ftwitter.png" alt="Twitter">
+                        <img src="images/ffacebook.png" alt="Facebook">
+                        <img src="images/fpring.png" alt="Print">
+                    </div>
+                    <div class="inline third hosted">
+                        <span>Handcrafted by</span><img src="images/yebo.png" alt="Yebo">
+                    </div>
+                </div>
+        </footer>
+        
+    </body>
+</html> 
